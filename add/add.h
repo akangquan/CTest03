795dbaca11d664da3309ca2a04b04f54ffd29c2d
kangquan <akangquan@qq.com>
2024-10-12 16:39:00 +0800

    master

---
+++ add/add.h
@@ -0,0 +1,8 @@
+#ifndef __ADD__
+#define __ADD__
+
+int add(int a,int b);
+
+
+
+#endif
---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include "add.h"
+
+int main()
+{
+    printf("Hello,world..");
+    printf("%d",add(3,9));
+}
+
+
+
